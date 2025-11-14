cea129c459c5ffc04d94652139f02d7199cb5e4a
panda <s22310282@student.unklab.ac.id>
2025-11-14 12:18:31 +0700

    Add Footer component

---
+++ src/Footer/index.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 bg-gray-200 mt-6 rounded text-center text-sm">
+      © 2025 React Introduction FE-C
+    </footer>
+  );
+}
